d8d48b78a1297e73ed03391e95f3a25386a6afe9
Pascal <pnieters@uos.de>
2021-01-14 12:16:22 +0100

    Loading JLD2: utils has a utility to load old initial weights

--- src/GeneralizedDynamicsFromData.jl
+++ src/GeneralizedDynamicsFromData.jl
@@ -8,6 +8,9 @@ using Plots
 using StatsPlots
 using Statistics
 using OrderedCollections
+using FileIO
+
+using JLD2
 
 @reexport using OrdinaryDiffEq
 @reexport using DiffEqFlux
@@ -16,6 +19,7 @@ using OrderedCollections
 export regression_model, reshape_all_parameters, reshape_parameters, restructure_parameters 
 export CallbackLog
 export repeat_experiment, grid_experiment
+export InitializationLoader
 include("utils.jl")
 
 export truscott_brindley, truscott_brindley1, selkov, selkov_sigma2, ensemble_selkov
