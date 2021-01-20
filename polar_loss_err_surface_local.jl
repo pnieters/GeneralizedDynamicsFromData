5e5a114eef76c013f716e9e6d3716502617c2ce9
Pascal <pnieters@uos.de>
2021-01-20 13:44:17 +0100

    plotting; but bugged calculations

--- polar_loss_err_surface_local.jl
+++ polar_loss_err_surface_local.jl
@@ -57,3 +57,38 @@ alpha_ax = Dict()
 for α in alpha
     alpha_ax[α] = ((1-α) .* error_surface_nld) .+ (α .* error_surface_cd)
 end
+
+alpha_ax_str = Dict([(str(k), alpha_ax[k]) for k in keys(alpha_ax)])
+
+using GLMakie
+using AbstractPlotting
+using AbstractPlotting.MakieLayout
+using ColorSchemes
+
+colors = ColorSchemes.berlin.colors
+
+s = slider(LinRange(0, 1.0, 101), raw=true, camera = campixel!, start = 0.0)
+
+data = lift(s[end][:value]) do v
+    alpha_ax[v]
+end
+
+# interactions don't really work.
+interactive = contour(w1_ax, w2_ax, data, linewidth = 0.2, levels=50, fillrange=true, colormap=colors)
+
+# cm = colorlegend(scene[end], raw=true, camera=campixel!, width= (30, 540))
+# scene = surface(w1_ax, w2_ax, data)
+outer_padding = 30
+scene, layout = layoutscene(
+    outer_padding, 
+    resolution = (500, 2500),
+    backgroundcolor = RGBf0(0.98, 0.98, 0.98)
+)
+plot_alphas = [0.0, 0.25, 0.5, 0.75, 1.0]
+for (plt_id, pa) in enumerate(plot_alphas)
+    ax = layout[plt_id, 1] = LAxis(scene, title="$(1-pa)*NLD + $(pa)*CD")
+    contour!(ax, w1_ax, w2_ax, alpha_ax[pa], linewidth=0.2, levels=50, fillrange=true, colormap=colors)
+    scatter!(ax, (0.0, 0.1))
+    ax.xlabel = "w_x"
+    ax.ylabel = "w_y"
+end
