ed5bb9fea77761f844450e72b8b83f5098658ccb
Pascal <pnieters@uos.de>
2021-02-22 19:52:07 +0100

    repetitions experiments for statistics

---
+++ fritzhugh_nagumo_statistics.jl
@@ -0,0 +1,53 @@
+using GeneralizedDynamicsFromData
+using OrderedCollections
+
+experiment_name = "fritzhugh_nagumo_statistics"
+repetitions = 1
+
+weight_decay = 1e-4
+η = 1e-1
+η_decay_rate = 0.5
+η_decay_step = 100
+η_limit = 1e-4
+
+loss_weights = [1/2, 1/2]
+
+construct_optimiser() = Flux.Optimiser(WeightDecay(weight_decay), 
+                                       ExpDecay(η,η_decay_rate,η_decay_step,η_limit), 
+                                       ADAM())
+construct_loss(θ, y, predict) = polar_loss(θ, y, loss_weights, predict)
+
+net_config = OrderedDict([
+    :inputs => 2,
+    :outputs => 1,
+    :neurons => 16,
+    :layers => 1,
+    :non_lin => tanh,
+    :initialization => Flux.glorot_normal
+]) 
+
+problem_one_stable = Dict([:equation => roessler,
+                           :parameters => Float32[0.9, 0.5, 1.2, 1.25],
+                           :u0 => Float32[-2.0, -0.5],
+                           :tspan => (0.0f0, 5.0f0),
+                           :ts => 0.1,
+                           :solver => Tsit5,
+                           :optimizer => construct_optimiser,
+                           :max_iter => 1000,
+                           :loss => construct_loss]
+                      )
+
+summary1s, callbacks1s = repeat_experiment(problem_one_stable, net_config, repetitions; ε = 0.01)
+
+problem_two_stable = Dict([:equation => roessler,
+                           :parameters => Float32[0.9, 0.5, 1.9, 1.25],
+                           :u0 => Float32[-2.0, -0.5],
+                           :tspan => (0.0f0, 5.0f0),
+                           :ts => 0.1,
+                           :solver => Tsit5,
+                           :optimizer => construct_optimiser,
+                           :max_iter => 1000,
+                           :loss => construct_loss]
+                         )
+
+summary2s, callbacks2s = repeat_experiment(problem_two_stable, net_config, repetitions; ε = 0.01)
---
+++ roessler_statistics.jl
@@ -0,0 +1,53 @@
+using GeneralizedDynamicsFromData
+using OrderedCollections
+
+experiment_name = "roessler_statistics"
+repetitions = 1
+
+weight_decay = 1e-4
+η = 1e-1
+η_decay_rate = 0.5
+η_decay_step = 100
+η_limit = 1e-4
+
+loss_weights = [1/2, 1/2]
+
+construct_optimiser() = Flux.Optimiser(WeightDecay(weight_decay), 
+                                       ExpDecay(η,η_decay_rate,η_decay_step,η_limit), 
+                                       ADAM())
+construct_loss(θ, y, predict) = polar_loss(θ, y, loss_weights, predict)
+
+net_config = OrderedDict([
+    :inputs => 3,
+    :outputs => 1,
+    :neurons => 16,
+    :layers => 1,
+    :non_lin => tanh,
+    :initialization => Flux.glorot_normal
+]) 
+
+problem_period1 = Dict([:equation => roessler,
+                        :parameters => Float32[0.1, 0.1, 4.0],
+                        :u0 => Float32[1.0, 1.0, 1.0],
+                        :tspan => (0.0f0, 10.0f0),
+                        :ts => 0.1,
+                        :solver => Tsit5,
+                        :optimizer => construct_optimiser,
+                        :max_iter => 1500,
+                        :loss => construct_loss]
+                      )
+
+summary1, callbacks1 = repeat_experiment(problem_period1, net_config, repetitions; ε = 0.01)
+
+problem_period1 = Dict([:equation => roessler,
+                        :parameters => Float32[0.1, 0.1, 6.0],
+                        :u0 => Float32[1.0, 1.0, 1.0],
+                        :tspan => (0.0f0, 10.0f0),
+                        :ts => 0.1,
+                        :solver => Tsit5,
+                        :optimizer => construct_optimiser,
+                        :max_iter => 1500,
+                        :loss => construct_loss]
+                      )
+
+summary2, callbacks2 = repeat_experiment(problem_period2, net_config, repetitions; ε = 0.01)
